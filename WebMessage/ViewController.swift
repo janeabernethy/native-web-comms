63328d5deb4cb60205f902925e6f4ed907dad7ba
Jane Abernethy <jane@canva.com>
2020-10-08 15:06:17 +1100

    Initial Commit

---
+++ WebMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebMessage
+//
+//  Created by Jane Abernethy on 8/10/20.
+//  Copyright © 2020 Jane Abernethy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
